ed2e80debf9c33898dfe7ed34e67e20d6827565d
prabal shrivastava <prabalshrivastava54@gmail.com>
2020-02-16 13:07:59 +0530

    first commit

---
+++ src/main/java/com/learning/spring/restful/microservices/restfulwebservices/RestfulWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring.restful.microservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulWebservicesApplication.class, args);
+	}
+
+}
